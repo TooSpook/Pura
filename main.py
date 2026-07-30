b0dcd44f59e1acb95fa00398b7ff282081d7b6fc
TooSpook <barnabasbozso05@gmail.com>
2026-07-30 14:06:37 +0200

    feat: file list

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("csvcleaner.pre-alpha.1.7")
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+    "csvcleaner.pre-alpha.1.7"
+)
 
 def main():
     app = QApplication([]) 
--- main_window.py
+++ main_window.py
@@ -1,10 +1,15 @@
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QListWidget
 from PyQt6.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("CSV cleaner")
+        self.setMinimumSize(QSize(512, 384))
+        self.setWindowIcon(QIcon("resources/app_icon.png"))
+        self.file_list = QListWidget()
 
         button = QPushButton("SELECT")
         button.setMinimumSize(120, 40)
@@ -12,14 +17,20 @@ class MainWindow(QMainWindow):
         button.setStyleSheet("padding: 12px 24px; font-size: 14px;")
         button.clicked.connect(self.file_selection)
 
-        self.setWindowTitle("CSV cleaner")
-        self.setMinimumSize(QSize(512, 384))
-        self.setWindowIcon(QIcon("resources/app_icon.png"))
-        self.setCentralWidget(button)
+        layout = QVBoxLayout()
+        layout.addWidget(self.file_list)
+        layout.addWidget(button)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
 
     def file_selection(self) -> str:
-        path, _ = QFileDialog.getOpenFileName(self, "Open CSV file", "", "CSV Files (*.csv);;All Files (*)")
+        path, _ = QFileDialog.getOpenFileName(
+            self, "Open CSV file", "", "CSV Files (*.csv);;All Files (*)"
+        )
+
         if not path:
             return
-        else:
-            return path
+        
+        self.file_list.addItem(path)
